5f515a041c0328fac7e4aaaf1659f9bf2073cdf9
arybary <l3fljl5ldydy@gmail.com>
2024-03-17 21:05:11 +0200

    ref console.log

--- src/app/certificate-manager/containers/certificate-manager/certificate-manager.component.ts
+++ src/app/certificate-manager/containers/certificate-manager/certificate-manager.component.ts
@@ -39,6 +39,5 @@ export class CertificateManagerComponent implements OnInit {
   }
   ngOnInit(): void {
     this.certificateFacade.loadCertificates();
-    console.log(this.certificates$);
   }
 }
